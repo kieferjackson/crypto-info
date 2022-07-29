27a5cd0a5cf8147a09c3b3998b620c3259e95038
kieferjackson <kieferleejackson@gmail.com>
2022-07-29 00:37:42 -0700

    Implemented dynamic queries for apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,51 @@
-var coin_paprika_url = 'https://api.coinpaprika.com/v1/coins/btc-bitcoin'; // eth-ethereum
-var coin_gecko_value_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017'; // coins/list gives the list of supported cryptocurrencies
-var coin_gecko_versus_url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
+function generate_data () {
+    const base_paprika_url = 'https://api.coinpaprika.com/v1/coins/'; // e.g. eth-ethereum, btc-bitcoin
+    const base_gecko_value_url = 'https://api.coingecko.com/api/v3/coins/'; // coins/list gives the list of supported cryptocurrencies
+    // var coin_gecko_versus_url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
+    
+    let current_crypto = 
+    {
+        fid: 'btc-bitcoin', // Full Crypto ID
+        id:  'bitcoin'      // Short Crypto ID
+    };
+    
+    // Change the Paprika url to query for the currently selected cryptocurrency
+    let ca_info_url = base_paprika_url + current_crypto.fid;
+    
+    // Generate the additional information for the selected cryptocurrency
+    get_api_data(ca_info_url, 'ADDITIONAL_INFO');
+    
+    // Generate the historical data object
+    const number_of_time_points = 12;
+    let historical_data = [];
+    
+    
+    // Iterate for each time point (tp)
+    for (var tp = 0 ; tp < number_of_time_points ; tp++) 
+    {
+        // Define historical data url
+        let hd_url = `${base_gecko_value_url}${current_crypto.id}/history?date=01-${tp + 1}-2017`;
+        historical_data[tp] = get_api_data(hd_url, 'HISTORICAL_DATA');
+    }
+    
+    console.log(historical_data);
+    
+    // get_api_data(coin_gecko_versus_url);
+}
 
-function getApi(requested_url) 
+function get_api_data(requested_url, data_to_generate) 
 {
-    fetch(requested_url)
-      .then( (response) => {
-        // Log the requested url address
-        console.log(`Data from: ${requested_url}`);
-        
-        return response.json();
-      })
-      .then( (data) => {
-        console.log(data);
-        if (requested_url === coin_paprika_url) 
-        {
+fetch(requested_url)
+    .then( (response) => {
+    // Log the requested url address
+    console.log(`Data from: ${requested_url}`);
+    
+    return response.json();
+    })
+    .then( (data) => {
+    console.log(data);
+    switch (data_to_generate) {
+        case 'ADDITIONAL_INFO':
             let additional_info = 
             {
                 name: data.name,
@@ -25,29 +56,18 @@ function getApi(requested_url)
 
             console.log('Additional Info: ');
             console.log(additional_info);
-        }
-        else if (requested_url === coin_gecko_value_url) 
-        {
-            const number_of_time_points = 12;
-            let historical_data = [];
 
-            for (var i = 0 ; i < number_of_time_points ; i++) 
-            {
-                historical_data[i] = 
-                {
-                    price: data.market_data.current_price.usd,
-                }
-            }
-            
+            return additional_info;
+
+        case 'HISTORICAL_DATA':
+            let data_pt = data.market_data.current_price.usd;
             console.log('Historical Data: ');
-            console.log(historical_data);
-        }
-        
-      });
-  }
-  
-  getApi(coin_paprika_url);
-  
-  getApi(coin_gecko_value_url);
-  
-  getApi(coin_gecko_versus_url);
+            console.log(data_pt);
+            
+            return data_pt;
+    }
+    
+    });
+}
+
+generate_data();
