d1408d7d87b5a65833e847c7381d144241f73d6d
kieferjackson <kieferleejackson@gmail.com>
2022-07-28 21:26:13 -0700

    Created additional info and historical data objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,47 @@ var coin_paprika_url = 'https://api.coinpaprika.com/v1/coins/btc-bitcoin'; // et
 var coin_gecko_value_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017'; // coins/list gives the list of supported cryptocurrencies
 var coin_gecko_versus_url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
 
-function getApi(requested_url) {
+function getApi(requested_url) 
+{
     fetch(requested_url)
-      .then(function (response) {
+      .then( (response) => {
         // Log the requested url address
-        console.log(`Data from: ${requested_url}`)
+        console.log(`Data from: ${requested_url}`);
         
         return response.json();
       })
-      .then(function (data) {
+      .then( (data) => {
         console.log(data);
+        if (requested_url === coin_paprika_url) 
+        {
+            let additional_info = 
+            {
+                name: data.name,
+                description: data.description,
+                website: data.links.website[0],
+                source_code: data.links.source_code[0]
+            }
+
+            console.log('Additional Info: ');
+            console.log(additional_info);
+        }
+        else if (requested_url === coin_gecko_value_url) 
+        {
+            const number_of_time_points = 12;
+            let historical_data = [];
+
+            for (var i = 0 ; i < number_of_time_points ; i++) 
+            {
+                historical_data[i] = 
+                {
+                    price: data.market_data.current_price.usd,
+                }
+            }
+            
+            console.log('Historical Data: ');
+            console.log(historical_data);
+        }
+        
       });
   }
   
